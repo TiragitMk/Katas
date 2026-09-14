1889295ffce8277b8abf2e36ea10ee38b5225f80
Daniel C <danielcabfe@gmail.com>
2026-09-14 20:27:50 +0200

    Primer commit

---
+++ hola.py
@@ -0,0 +1,8 @@
+# Primer archivo de las Katas de Rolando.
+
+alumnos = ["Ana", "María", "Alfie", "Pedro", "Joaquín"]
+
+for i in alumnos:
+    print(i)
+    if i == "Pedro":
+        print("¿Cómo estás, Pedro?")
